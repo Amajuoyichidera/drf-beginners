d6abd1eee7e40280ed930e0d9ad51c687545120c
Amajuoyi chidera <amajuoyichideradavid@gmail.com>
2023-07-21 18:17:44 +0100

    first version

---
+++ api_basic/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView
+
+urlpatterns = [
+    # path('article/', article_list),
+    # path('detail/<int:pk>/', article_detail),
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
+]
+  
